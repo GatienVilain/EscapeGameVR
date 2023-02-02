94c9959af231e9a580bc7016fedbcb8eea982586
DorianLarouziere <dorian.larouziere@student.junia.com>
2023-02-02 17:18:36 +0100

    correction resize

--- Assets/Scripts/Taillevar.cs
+++ Assets/Scripts/Taillevar.cs
@@ -15,6 +15,10 @@ public class Taillevar : MonoBehaviour
     private float downPositionY;
     private float originalPositionY;
 
+    [SerializeField] private Transform rightHand;
+    [SerializeField] private Transform leftHand;
+
+
     [SerializeField] private float sizeDecrementValue = 0.35f;
     //private float timeleft;
 
@@ -64,21 +68,49 @@ public class Taillevar : MonoBehaviour
             if (versbas)
             {
                 transform.position -= new Vector3(0f, speed*Time.deltaTime , 0f);
+                rightHand.localScale -= speed * Time.deltaTime * new Vector3(1, 1, 1);
+                leftHand.localScale -= speed * Time.deltaTime * new Vector3(1, 1, 1);
                 if (transform.position.y <= downPositionY)
                 {
-                    transform.position = new Vector3(0f, downPositionY, 0f);
+                    transform.position = new Vector3(transform.position.x, downPositionY, transform.position.z);
                     versbas = false;
-                    bas=true;
+                    bas = true;
+                    
+ 
                 }
+
+                if (rightHand.localScale.x <= 0.5f)
+                {
+                    rightHand.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    leftHand.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                }
+
+                if (rightHand.localScale.x <= 0.5f && transform.position.y <= downPositionY)
+                {
+                    vershaut = false;
+                }
+
             }
             if (vershaut)
             {
                 transform.position += new Vector3(0f, speed*Time.deltaTime , 0f);
+                rightHand.localScale += new Vector3(speed*Time.deltaTime, speed * Time.deltaTime, speed * Time.deltaTime);
+                leftHand.localScale += new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, speed * Time.deltaTime);
+                bas = false;
                 if (transform.position.y >= originalPositionY)
                 {
-                    transform.position = new Vector3(0f, originalPositionY, 0f);
+                    transform.position = new Vector3(transform.position.x, originalPositionY, transform.position.z);                
+                }
+
+                if(rightHand.localScale.x >= 1f)
+                {
+                    rightHand.localScale = new Vector3(1f, 1f, 1f);
+                    leftHand.localScale = new Vector3(1f, 1f, 1f);
+                }
+
+                if(rightHand.localScale.x >= 1f && transform.position.y >= originalPositionY)
+                {
                     vershaut = false;
-                    bas=false;
                 }
             }
         }
