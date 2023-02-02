0621c581337511d1675a73a05ce1b60990ef6a1d
Giraud-Pierre <90455112+Giraud-Pierre@users.noreply.github.com>
2023-02-02 14:39:32 +0100

    bowUpdate

---
+++ Assets/Bow/Scripts/ArrowControler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowControler : MonoBehaviour
+{
+    [SerializeField] private GameObject midPointVisual, arrowPrefab, arrowSpawnPoint;
+    [SerializeField] private float arrowMaxSpeed = 10f;
+
+    [SerializeField] private AudioSource bowReleaseAudioSource;
+
+    public void PrepareArrow()
+    {
+        midPointVisual.SetActive(true);
+    }
+    public void  ReleaseArrow(float strength)
+    {
+        bowReleaseAudioSource.Play();
+        midPointVisual.SetActive(false);
+        
+        GameObject arrow = Instantiate(arrowPrefab);
+        arrow.transform.position = arrowSpawnPoint.transform.position;
+        arrow.transform.rotation = midPointVisual.transform.rotation;
+        Rigidbody rb = arrow.GetComponent<Rigidbody>();
+        rb.AddForce(midPointVisual.transform.forward * strength * arrowMaxSpeed, ForceMode.Impulse);
+    }
+
+}
