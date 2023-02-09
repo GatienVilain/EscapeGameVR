84c4f75a8213be914ef922739ef47f26d044bcb1
Giraud-Pierre <90455112+Giraud-Pierre@users.noreply.github.com>
2023-02-09 17:44:28 +0100

    MenuCorrection

--- Assets/Scripts/Menus/MainMenuController.cs
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.GraphicsBuffer;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private GameObject mainCamera;
     [SerializeField] private GameObject levelWindow;
     [SerializeField] private GameObject settingsWindow;
+    private bool isSet = false;
+
+    IEnumerator SetMenu()
+    {
+        yield return new WaitForSeconds(1);
+        Vector3 position = new Vector3(mainCamera.transform.forward.x, -0.1f, mainCamera.transform.forward.z);
+        transform.position = mainCamera.transform.position + 2 * position;
+
+        transform.rotation = Quaternion.LookRotation(transform.position - mainCamera.transform.position);
+    }
 
     void Start()
     {
-        Vector3 position = new Vector3(mainCamera.transform.forward.x, 0, mainCamera.transform.forward.z);
-        transform.position = mainCamera.transform.position + position;
+        StartCoroutine(SetMenu());
     }
 
     public void GetLevelsWindow()
