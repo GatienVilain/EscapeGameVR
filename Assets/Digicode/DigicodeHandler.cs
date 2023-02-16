b5d83fa68bd6aaa06a1ca0faf5566b249de2ece3
Giraud-Pierre <90455112+Giraud-Pierre@users.noreply.github.com>
2023-02-16 14:13:13 +0100

    PressButton

--- Assets/Digicode/DigicodeHandler.cs
+++ Assets/Digicode/DigicodeHandler.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DigicodeHandler : MonoBehaviour
 {
+    [SerializeField] private string correctCode = "1234";
+    [SerializeField] private TextMeshPro display;
+    private string currentCode;
+
     // Start is called before the first frame update
     void Start()
     {
-        int code =
+        currentCode = "";
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateDisplay()
     {
-        
+
+    }
+
+    public void PressButton(int number)
+    {
+        currentCode += number.ToString();
+        if (currentCode.Length == correctCode.Length)
+        {
+            if (currentCode == correctCode)
+            {
+                //Code is correct
+            }
+            else
+            {
+                //BEEEP code incorrect
+            }
+            currentCode = "";
+        }
     }
 }
