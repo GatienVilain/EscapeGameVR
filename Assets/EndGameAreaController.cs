80121017f8330659d83c977f96cae0295338c564
Giraud-Pierre <90455112+Giraud-Pierre@users.noreply.github.com>
2023-02-27 22:18:05 +0100

    EndGameScene2

--- Assets/EndGameAreaController.cs
+++ Assets/EndGameAreaController.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGameAreaController : MonoBehaviour
 {
-    [SerializeField] GameObject prefabEndGame;
-    [SerializeField] private GameObject mainCamera = default;
-    [SerializeField] private GameObject endGameWindow = default;
-    [SerializeField] private GameObject canvas = default;
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
@@ -16,11 +13,7 @@ public class EndGameAreaController : MonoBehaviour
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             //Debug.Log("EndGame");
-            canvas.GetComponent<PauseMenuController>().SetMenuPosition();
-            endGameWindow.SetActive(true);
-            Time.timeScale = 0;
-
+            SceneManager.LoadScene("EndMenu");
         }
-
     }
 }
