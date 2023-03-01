23a5c321e3af6e48ddb6bf2d1878a1527f6533a1
Gatien Vilain <gatien.vilain@outlook.fr>
2023-03-01 12:25:46 +0100

    Comment stopTransition

--- Assets/Scripts/Audio/MusicsManager.cs
+++ Assets/Scripts/Audio/MusicsManager.cs
@@ -100,9 +100,6 @@ public class MusicsManager : MonoBehaviour
 
     private IEnumerator SoftTransition(AudioClip newMusic)
     {
-        // Si on relance la musique parce que la scene a changé, on n'arrête pas cette transition
-        stopTransition = false;
-
         // On récupère la source audio qui joue actuellement et la source audio qui va jouer la nouvelle musique
         AudioSource fadingTrack;
         AudioSource newTrack;
@@ -120,6 +117,15 @@ public class MusicsManager : MonoBehaviour
         // On donne la nouvelle musique à la source audio qui va la jouer
         newTrack.clip = newMusic;
         newTrack.Play();
+
+        // S’il y a une transition en cours et que la scene change, on fait en sorte que la nouvelle coroutine puisse continuer de se jouer.
+        // Explication:
+        //      S’il y a une transtion en cours et que la scene change, on se retrouve avec deux coroutines qui fonctionnent en même temps.
+        //      Il faut donc arrêter la première et laisser la seconde continuer.
+        //      Pour cela, on met stopTransition à true juste avant de lancer la nouvelle coroutine (voir Update).
+        //  	Et comme la première coroutine se trouve dans la boucle while, elle va arrêter la transition en cours. (voir la boucle while ci-dessous)
+        //  	Enfin, on met stopTransition à false pour que la nouvelle coroutine puisse continuer de se jouer.
+        stopTransition = false;
 
         // On fait une transition entre les deux musiques
         float percentage = 0;
